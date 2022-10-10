09df22f5e34a7bc30159226fb0359722ed9debad
luangelsdorf <luanferreira2136@gmail.com>
2022-10-10 17:30:46 -0300

    Final first page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+import '../public/css/bootstrap-grid.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
