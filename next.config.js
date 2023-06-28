0d6c194ae20b892af547b413adeebe44a2ff29f8
luangelsdorf <luanferreira2136@gmail.com>
2023-06-28 19:49:20 -0300

    PostCard component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['casulo.pet']
+  },
 
   webpack(config) {
     config.module.rules.push({
---
+++ src/components/blog/PostCard/index.js
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { apiURL } from 'src/utils/env';
+import { getExcerpt } from 'src/utils/helpers';
+import styles from './PostCard.module.scss';
+import Arrow from 'public/images/icons/ui/arrow-right.svg';
+import Button from 'src/components/common/Button';
+
+export default function PostCard(props) {
+  const category = props.categories.data[0].attributes;
+
+  return (
+    <article className={styles.card}>
+
+      <div>
+        <div className={styles.badge}>
+          <Link className="btn sm" href={`/blog/categorias/${category.slug}`}>
+            {category.name}
+          </Link>
+        </div>
+        <Link href={`/blog/posts/${props.slug}`} className={`link-image ${styles.thumbnail}`}>
+          <div>
+            <Image
+              fill
+              src={apiURL + props.cover.data.attributes.url}
+              alt="Thumbnail"
+              sizes="
+                  (max-width: 576px) 100vw,
+                  (max-width: 992px) 50vw, 33vw
+              "
+            />
+          </div>
+        </Link>
+      </div>
+
+      <div className={styles.body}>
+        <Link className="link-image" href={`/blog/posts/${props.slug}`}>
+          <h3>{props.title}</h3>
+        </Link>
+        <p>
+          {props.body ? getExcerpt(props.body) : null}
+        </p>
+        <Button className="transparent sm">Ler Mais</Button>
+      </div>
+    </article>
+  )
+}
