0c4479c28cc11d5cdcbcbfcc4f84e655c8ae0e28
luangelsdorf <luanferreira2136@gmail.com>
2023-06-30 14:24:51 -0300

    PostList section

---
+++ src/components/blog/PostList/index.js
@@ -0,0 +1,30 @@
+import Button from 'src/components/common/Button';
+import PostCard from '../PostCard';
+import styles from './PostList.module.scss';
+/* import Arrow from 'public/images/icons/ui/arrow-right.svg'; */
+
+export default function PostList({ posts }) {
+  return (
+    <div className={styles.section}>
+      <div className="container">
+        <div className="row justify-content-center">
+          {
+            posts.map((post, index) => (
+              <div className="col-12 col-lg-4" key={index}>
+                <PostCard {...post.attributes} />
+              </div>
+            ))
+          }
+          {
+            posts.map((post, index) => (
+              <div className="col-12 col-lg-4" key={index}>
+                <PostCard {...post.attributes} />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+      <Button btnElement>Ver Mais Publicações</Button>
+    </div>
+  )
+}
