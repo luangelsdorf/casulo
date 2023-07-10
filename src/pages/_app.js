4cdd703bd3898439a0820dd28583650fd5935b51
luangelsdorf <luanferreira2136@gmail.com>
2023-07-10 09:30:17 -0300

    Cookies popup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ import 'public/css/ckeditor.min.css';
 import 'src/styles/styles.scss';
 import { Calistoga } from 'next/font/google';
 import { Nunito_Sans } from 'next/font/google';
+import Cookies from 'src/components/common/Cookies';
 
 const calistoga = Calistoga({ subsets: ['latin'], weight: ['400'] });
 const nunito = Nunito_Sans({ subsets: ['latin'], weight: ['400', '600', '700'] });
@@ -23,6 +24,7 @@ function MyApp({ Component, pageProps }) {
       </style>
 
       <Component {...pageProps} />
+      <Cookies />
     </>
   )
 }
