414c33952a77597909dfb23afa0888a5fa94573a
luangelsdorf <luanferreira2136@gmail.com>
2023-11-27 16:42:44 -0300

    Cases listing update

--- src/components/cases/List/index.js
+++ src/components/cases/List/index.js
@@ -8,9 +8,15 @@ export default function List({ cases }) {
   const [list, setList] = useState(cases);
 
   function handleClick(e) {
+    let { filterField, filterValue } = e.currentTarget.dataset;
+
+    if (filterValue === 'all') {
+      setList(cases);
+      return;
+    }
+
     e.currentTarget.parentElement.childNodes.forEach(el => el.classList.remove('inverted'));
     e.currentTarget.classList.add('inverted');
-    let { filterField, filterValue } = e.currentTarget.dataset;
     let newList = cases.filter(item => item.attributes[filterField] === filterValue);
     setList(newList);
   }
@@ -19,6 +25,7 @@ export default function List({ cases }) {
     <div className={styles.section}>
       <div className="container">
         <div className={styles.filters} style={{ color: 'black' }}>
+          <Button onClick={handleClick} data-filter-value="all" className="sm" btnElement>Todos</Button>
           <Button onClick={handleClick} data-filter-field="sex" data-filter-value="Macho" className="sm" btnElement>♂</Button>
           <Button onClick={handleClick} data-filter-field="sex" data-filter-value="Fêmea" className="sm" btnElement>♀</Button>
           <Button onClick={handleClick} data-filter-field="size" data-filter-value="Filhote" className="sm" btnElement>Filhote</Button>
@@ -31,7 +38,7 @@ export default function List({ cases }) {
           {
             list.length > 0 ? (
               cases.map((card, index) => (
-                <div className="col-12 col-lg-4 gy-4" key={index} data-display={list.some(dog => dog.attributes.slug === card.attributes.slug)}>
+                <div className="col-12 col-lg-4 col-xl-6 gy-4" key={index} data-display={list.some(dog => dog.attributes.slug === card.attributes.slug)}>
                   <Link href={`/cases/${card.attributes.slug}`}>
                     <TestimonialCard {...card.attributes} short />
                   </Link>
