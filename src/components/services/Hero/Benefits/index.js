d38809cb0c5e95c3bdd2742a5ab5e935de4ca825
luangelsdorf <luanferreira2136@gmail.com>
2023-12-02 13:11:02 -0300

    Benefits component

---
+++ src/components/services/Hero/Benefits/index.js
@@ -0,0 +1,27 @@
+import styles from './Benefits.module.scss';
+import Check from 'public/images/icons/ui/check-circle.svg';
+
+export default function Benefits({ service, content }) {
+  console.log(content);
+  console.log(service);
+  return (
+    <div className={`${styles.benefits} ${styles[service]}`}>
+      <h2>{content.headline.title}</h2>
+      {content.headline.text && <p>{content.headline.text}</p>}
+      <ul>
+        {
+          content.items.map((item, i) => (
+            <li key={i}>
+              <Check />
+              <div>
+                <h3>{item.title}</h3>
+                {item.text && <p>{item.text}</p>}
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+      {content.additionalText && <p>{content.additionalText}</p>}
+    </div>
+  )
+}
